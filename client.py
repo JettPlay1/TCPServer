eb6946770cd6478b6b2e1bd39157dee193a8f276
JettPlay1 <jettplayyt@gmail.com>
2024-06-11 20:56:27 +0500

    Made comments

--- client.py
+++ client.py
@@ -13,13 +13,18 @@ def send_request(command, server_host, server_port):
 
 
 if __name__ == "__main__":
+
+    # Читаем аргумент коммандной строки
     if len(sys.argv) < 2:
-        print("Usage: python client.py <command> <params>")
+        print("Usage: python client.py <command>")
+        print("For example: {\"command1\":\"CheckLocalFile\",\"params\":{\"file_path\":\"my_file\",\"signature\":\"test\"}}")
         sys.exit(1)
-    print(sys.argv[1])
+
+    # Парсим JSON 
     command = json.loads(sys.argv[1])
     SERVER_HOST = "localhost"
     SERVER_PORT = 9999
     
+    # Отправляем запрос и получаем ответ от сервера
     response = send_request(command, SERVER_HOST, SERVER_PORT)
     print("Response from server:", response)
