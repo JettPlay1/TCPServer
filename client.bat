e53fa34195826c75e24ec9ba87c40421198683b9
JettPlay1 <jettplayyt@gmail.com>
2024-06-11 21:57:41 +0500

    Deleted client.bat

--- client.bat
+++
@@ -1,3 +0,0 @@
-python client.py {\"command1\":\"CheckLocalFile\",\"params\":{\"file_path\":\"server.py\",\"signature\":\"BUFFER\"}}
-python client.py {\"command1\":\"QuarantineLocalFile\",\"params\":{\"file_path\":\"testfile\"}} 
-pause
